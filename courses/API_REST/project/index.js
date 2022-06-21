1d96ea5fd62d174387ada5cb477cd1cd8097e7c3
Kevin J. Zea <kevinjzea@gmail.com>
2022-06-20 23:11:46 -0500

    Added an option to upload images to the API.

--- courses/API_REST/project/index.js
+++ courses/API_REST/project/index.js
@@ -1,11 +1,15 @@
 const API_KEY = '4e82d29e-0132-46e1-b89a-fc019e471a8a';
 const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2';
 const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites';
+const API_URL_UPLOAD = 'https://api.thecatapi.com/v1/images/upload';
 
 const randomMichis = document.getElementById('random-michis');
 const favouriteMichis = document.getElementById('favourite-michis');
 const newMichisButton = document.getElementById('new-michis-button');
 const errorSpan = document.getElementById('error-span');
+
+const form = document.getElementById('uploading-form');
+const previewContainer = document.getElementById('preview-container');
 
 const createElement = (type, properties = {}) => {
   const newElement = document.createElement(type);
@@ -49,10 +53,16 @@ const createNewImgContainer = (containerId, imageUrl, imageId) => {
   });
 };
 
-const getCat = async (container, url, api_key) => {
+const getCat = async (container, url, apiKey) => {
   try {
     const response = await fetch(
-      `${url}${api_key ? `?api_key=${api_key}` : ''}`
+      url,
+      apiKey && {
+        method: 'GET',
+        headers: {
+          'x-api-key': apiKey,
+        },
+      }
     );
     const data = await response.json();
 
@@ -81,13 +91,13 @@ const getCat = async (container, url, api_key) => {
   }
 };
 
-const removeFromFavourites = async (imageId, url, api_key) => {
-  const response = await fetch(
-    `${url}/${imageId}${api_key ? `?api_key=${api_key}` : ''}`,
-    {
-      method: 'DELETE',
-    }
-  );
+const removeFromFavourites = async (imageId, url, apiKey) => {
+  const response = await fetch(`${url}/${imageId}`, {
+    method: 'DELETE',
+    headers: {
+      'x-api-key': apiKey,
+    },
+  });
 
   const data = await response.json();
 
@@ -98,19 +108,17 @@ const removeFromFavourites = async (imageId, url, api_key) => {
   getCat(favouriteMichis, API_URL_FAVOURITES, API_KEY);
 };
 
-const addToFavourites = async (imageId, url, api_key) => {
-  const response = await fetch(
-    `${url}${api_key ? `?api_key=${api_key}` : ''}`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        image_id: imageId,
-      }),
-    }
-  );
+const addToFavourites = async (imageId, url, apiKey) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': apiKey,
+    },
+    body: JSON.stringify({
+      image_id: imageId,
+    }),
+  });
 
   const data = await response.json();
 
@@ -119,6 +127,43 @@ const addToFavourites = async (imageId, url, api_key) => {
   }
 
   getCat(favouriteMichis, API_URL_FAVOURITES, API_KEY);
+};
+
+const uploadMichiPhoto = async () => {
+  const response = await fetch(API_URL_UPLOAD, {
+    method: 'POST',
+    headers: {
+      'x-api-key': API_KEY,
+    },
+    body: formData,
+  });
+
+  const data = await response.json();
+
+  if (response.status !== 200) {
+    errorSpan.innerHTML = `Hubo un error al subir el michi: ${response.status} ${data.message}`;
+    return;
+  }
+
+  addToFavourites(data.id, API_URL_FAVOURITES, API_KEY);
+};
+
+const showPreview = () => {
+  const formData = new FormData(form);
+  previewContainer.innerHTML = '';
+
+  if (formData.get('file').type.includes('image')) {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const newImg = createElement('img', {
+        alt: 'Preview of just uploaded image',
+        height: '200',
+        src: event.target.result,
+      });
+      previewContainer.appendChild(newImg);
+    };
+    reader.readAsDataURL(formData.get('file'));
+  }
 };
 
 newMichisButton.addEventListener('click', () =>
