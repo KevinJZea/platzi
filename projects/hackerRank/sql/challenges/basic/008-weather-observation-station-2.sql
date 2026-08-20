b9a41c539014f22f43751e75eb850d6245bdc719
kevinjzea <kevinjzea@gmail.com>
2026-08-20 17:32:58 -0600

    HackerRank - First 8 SQL Challenges

---
+++ projects/hackerRank/sql/challenges/basic/008-weather-observation-station-2.sql
@@ -0,0 +1,11 @@
+/* 
+
+Query the following two values from the STATION table:
+
+1. The sum of all values in LAT_N rounded to a scale of  decimal places.
+2. The sum of all values in LONG_W rounded to a scale of  decimal places.
+
+*/
+
+SELECT CITY, STATE
+FROM STATION;
